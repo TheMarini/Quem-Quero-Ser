69a2bb3fdf9f082ed708b48ae93e6f18292e67ff
Bruno Marini <bruno.marini@hotmail.com>
2017-08-13 00:20:12 -0300

    Smooth Scrol Link | Menu Item:hover (bug fix) | Nuvem add | auto Nav Items color (Js)

--- js/script.js
+++ js/script.js
@@ -3,19 +3,28 @@ var $secoes = [0, 1, 2, 3, 4, 5];
 var $secaoAtual;
 
 $(document).ready(function () {
+    /* Nav */
+    $("#menu li").each(function (index) {
+        $(this).css('background-color', $colors[0]);
+        $(this).css('color', $colors[index + 1]);
+        $(this).css('border-color', $colors[index + 1]);
+    });
+
+
     $(window).scroll(function () {
         /* Distância do scroll até o top da tela */
         var $scrollTop = $(this).scrollTop();
         var $mainTop = $('#Main').offset().top;
         var $menuTop = $('#menu').offset().top;
 
-        var $planosTop = $('#Planos').offset().top;
-        var $artigosTop = $('#Artigos').offset().top;
+
+        var $metodosTop = $('#Metodos').offset().top;
         var $equipeTop = $('#Equipe').offset().top;
+        var $recomendadosTop = $('#Recomendados').offset().top;
         var $contatoTop = $('#Contato').offset().top;
 
 
-        if ($scrollTop >= 100) {
+        if ($scrollTop > 0) {
             $secaoAtual = 0;
             navIndex();
 
@@ -34,9 +43,9 @@ $(document).ready(function () {
                     $('#Nav #home').removeClass('hide');
                     $('#Nav .redes').removeClass('hide');
 
-                    if ($scrollTop >= $artigosTop - 100) {
-                        if ($scrollTop >= $planosTop - 100) {
-                            if ($scrollTop >= $equipeTop - 100) {
+                    if ($scrollTop >= $metodosTop - 100) {
+                        if ($scrollTop >= $equipeTop - 100) {
+                            if ($scrollTop >= $recomendadosTop - 100) {
                                 if ($scrollTop >= $contatoTop - 300) {
                                     $secaoAtual = 4;
                                     navIndex();
@@ -54,6 +63,15 @@ $(document).ready(function () {
                             navIndex();
                         }
                     }
+                } else {
+                    $('#Nav').removeClass('nav-fixed');
+                    $('#Nav').addClass('nav-start');
+
+                    $('#menu').removeClass('menu-fixed');
+                    $('#menu').addClass('menu-start');
+
+                    $('#Nav #home').addClass('hide');
+                    $('#Nav .redes').addClass('hide');
                 }
             }
         } else {
@@ -65,40 +83,30 @@ $(document).ready(function () {
             $('#menu').find("li").eq(0).css('color', $colors[1]);
         }
 
-
-        if ($scrollTop < $mainTop) {
-            $('#Nav').removeClass('nav-fixed');
-            $('#Nav').addClass('nav-start');
-
-            $('#menu').removeClass('menu-fixed');
-            $('#menu').addClass('menu-start');
-
-            $('#Nav #home').addClass('hide');
-            $('#Nav .redes').addClass('hide');
-        }
-
     });
 
 
     $('#menu li').on('mouseenter', function () {
-        $('#Nav').css('background-color', $(this).css('border-top-color'));
+        var $itemIndex = $("li").index(this);
 
-        if ($("li").index(this) != $secaoAtual - 1) {
-            $(this).css('background-color', $(this).css('border-top-color'));
+        $('#Nav').css('background-color', $colors[$itemIndex + 1]);
+
+        if ($itemIndex != $secaoAtual) {
+            $(this).css('background-color', $colors[$itemIndex + 1]);
             $(this).css('color', $colors[0]);
+
+
+            $("li").eq($secaoAtual).css('background-color', $colors[0]);
+            $("li").eq($secaoAtual).css('color', $colors[$secaoAtual + 1]);
         }
 
-    });
-    $('#menu li').on('mouseleave', function () {
-
-        $('#Nav').css('background-color', $colors[0]);
-
-        if ($("li").index(this) != $secaoAtual - 1) {
-            $(this).css('background-color', $colors[0]);
-            $(this).css('color', $(this).css('border-top-color'));
-        }
+        $('#menu li').on('mouseleave', function () {
+            $('#Nav').css('background-color', $colors[0]);
+            navIndex();
+        });
     });
 
+    /* VÍDEO SOUND */
     $('#Video video').on('click', function () {
 
         if ($(this).prop('muted')) {
@@ -132,6 +140,43 @@ $(document).ready(function () {
         }
         first_time = false;
     });
+
+    /* SMOOTH SCROLL LINK*/
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
 });
 
 function navIndex() {
